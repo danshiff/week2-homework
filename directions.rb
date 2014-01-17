4db329ec5abe91f9dcfb9155e7b0ffe585223543
Daniel Shiff <shiff.dan.e@gmail.com>
2014-01-16 18:35:35 -0600

    forgot to save last time

--- directions.rb
+++ directions.rb
@@ -1,17 +1,21 @@
 require 'open-uri'
 require 'json'
 
+#Get origin, and format it
 print "Please enter your starting location: "
 start = gets
 start = start.chomp
 start = start.tr(" ", "+")
 
+#Get destination, format
 print "Please enter your desired destination: "
 dest = gets
 dest = dest.chomp
 dest = dest.tr(" ", "+")
 
+#Get info from google
 json_data = open("http://maps.googleapis.com/maps/api/directions/json?origin=#{start}&destination=#{dest}&sensor=true").read()
 data = JSON.parse(json_data)
+#Output info
 puts "Distance: #{data['routes'][0]['legs'][0]['distance']['text']}"
 puts "Travel time #{data['routes'][0]['legs'][0]['duration']['text']}:"
--- geolocate.rb
+++ geolocate.rb
@@ -1,12 +1,16 @@
 require 'open-uri'
 require 'json'
 
+#Get input, format it correctly
 print "Please enter an address, city, or zipcode (or well known location): "
 address = gets
 address = address.chomp
 address = address.tr(" ", "+")
 
+#Get info from google.
 json_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=true").read()
 data = JSON.parse(json_data)
+
+#Print the info.
 puts "Latitude: #{data['results'][0]['geometry']['location']['lat']}"
 puts "Longitude: #{data['results'][0]['geometry']['location']['lng']}"
